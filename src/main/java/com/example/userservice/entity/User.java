9eb1d40a63341bf42a2fdf5581689c67ca8adab0
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-02-10 16:40:37 +0700

    Created JPA entity

---
+++ src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Date creationDate;
+}
+
