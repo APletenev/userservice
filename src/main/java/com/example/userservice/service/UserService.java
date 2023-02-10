87baee967fe0d6c6ee2ffb08c521be95d1bd8668
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-02-10 20:00:37 +0700

    Created service layer

---
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.userservice.service;
+
+import com.example.userservice.entity.User;
+
+import java.util.Optional;
+
+public interface UserService {
+    User saveUser(User user);
+
+    Optional<User> getUserById(Long userId);
+}
---
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.userservice.service;
+
+import com.example.userservice.entity.User;
+import com.example.userservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long userId) {
+        return userRepository.findById(userId);
+    }
+}
