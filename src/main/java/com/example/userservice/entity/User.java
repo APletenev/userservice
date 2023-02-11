0492a74d099052c51d7f8bf26a1735576d5b224c
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-02-11 13:47:28 +0700

    Simple validation

--- src/main/java/com/example/userservice/entity/User.java
+++ src/main/java/com/example/userservice/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.userservice.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
@@ -18,9 +22,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @Email
+    @Column(unique=true)
     private String email;
+    @NotBlank
     private String password;
-    private Date creationDate;
+    @PastOrPresent
+    @NotNull
+    private LocalDate creationDate;
 }
 
