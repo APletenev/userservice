b14bd9932df6ac0492ebb05d0f5481dff52d19c6
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-02-14 21:30:39 +0700

    Added JavaDocs for api requests methods

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.Optional;
 
 @RestController
@@ -15,10 +14,21 @@ import java.util.Optional;
 public class UserController {
     private UserService userService;
 
+    /**
+     * Creates of a user account
+     * @param user User to create
+     * @return JSON with user properties
+     */
     @PostMapping("/create")
     public User create(@RequestBody @Valid User user) {
         return userService.saveUser(user);
     }
+
+    /**
+     * Get user by user id
+     * @param  id User id
+     * @return User if id is valid. Null if requested user id is not exists.
+     */
     @GetMapping("/{id}")
     public Optional<User> byUserId(@PathVariable Long id) {
         return userService.getUserById(id);
